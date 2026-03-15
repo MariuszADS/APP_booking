96cb9dd429f82914489598f1ce78fd38727f3e1b
MariuszADS <informatyka402@gmail.com>
2026-03-15 19:21:38 +0100

    API: works | added: utils_components | tailwindcss: to_fix

--- server/index.js
+++ server/index.js
@@ -4,43 +4,34 @@ import express from "express"
 import cors from "cors"
 import helmet from "helmet"
 
-const users = await prisma.user.findMany()
-const services = await prisma.service.findMany()
-const booking = await prisma.booking.findMany()
 
 const port = 8000
 const app = express()
 
 // Security middleware
-app.use(helmet(""))
+app.use(helmet())
 
 //CORS config
 app.use(cors({
-  origin: 'http://localhost:5173',
-  methods: ['GET', 'POST'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.get("/users", (res, req) => {
+app.get("/api/users", async (req, res) => {
+    const users = await prisma.user.findMany()
     res.json(users)
 })
 
-app.get('/users/:id', (req, res) => {
-    const user = users.find(u => u.id === parseInt(req.params.id));
-    if (!user) return res.status(404).json({ message: 'User not found' });
-    res.json(user);
-});
-
-app.get("/services", (res, req) => {
-    res.statusCode = 200
-    res.end(JSON.stringify({route:services}))
+app.get("/services",async(req,res)=>{
+    const services = await prisma.service.findMany()
+    await res.json(services)
 })
 
-app.get("/booking", (res, req) => {
-    res.statusCode = 200
-    res.end(JSON.stringify({route:booking}))
+app.get("/booking",async (req,res)=>{
+    const booking = await prisma.booking.findMany()
+    res.json(booking)
 })
-
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
