f59f3a9ef8aeb3f47f29d0c41982b7cd4b7c40b5
MariuszADS <informatyka402@gmail.com>
2026-03-17 22:05:54 +0100

    added: paths_handlers

--- server/routes/bookings_routes.js
+++ server/routes/bookings_routes.js
@@ -1,7 +1,19 @@
 import express from "express"
 
 const router = express.Router()
-//create endpoint
-router.get("/bookings_details",(res,req)=>{
-    res.send("<p>booking_details page!</p>")
+
+router.get("/api/bookings_details", (res, req) => {
+    res.json({ httpMethod: "get" })
 })
+
+// router.post("/api/bookings_details", (req, res) => {
+//     res.json({ httpMethod: "post" })
+// })
+
+// router.put("/api/bookings_details", (req, res) => {
+//     res.json({ httpMethod: "put" })
+// })
+
+// router.delete("/api/bookings_details", (req, res) => {
+//     res.json({ httpMethod: "delete" })
+// })
--- server/routes/services_routes.js
+++ server/routes/services_routes.js
@@ -1,10 +1,19 @@
 import express from "express"
-import { getBooking } from "../controllers/booking_controller"
+// import { getBooking } from "../controllers/booking_controller"
 
 const router = express.Router()
 // create endpoint | getBooking not in used
-router.get("/api/services", getBooking, (res, req) => {
-    res.send("<p>service_date_time_booking page!</p>")
+router.get("/api/services", (res, req) => {
+    res.json({ httpMethod: "get" })
+})
+router.post("/api/services", (res, req) => {
+    res.json({ httpMethod: "post" })
+})
+router.put("/api/services", (res, req) => {
+    res.json({ httpMethod: "put" })
+})
+router.delete("/api/services", (res, req) => {
+    res.json({ httpMethod: "delete" })
 })
 
 export default router
